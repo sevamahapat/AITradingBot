283fb63ef620acfe0697979c0e055b98e2d525a8
Seva Mahapatra <smahapat@andrew.cmu.edu>
2025-05-09 17:13:42 -0400

    Added date parsing logic

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,13 @@
 import pandas as pd
+from dateutil import parser
+
+def parse_date_safe(val):
+    try:
+        if pd.notnull(val):
+            return parser.parse(val, dayfirst=False)  # handles both DD-MM-YYYY and MM/DD/YYYY
+    except Exception:
+        return pd.NaT
+    return pd.NaT
 
 def clean_amount(val):
     if isinstance(val, str):
@@ -12,9 +21,11 @@ def load_trading_csv(filepath: str) -> pd.DataFrame:
     df = pd.read_csv(filepath)
 
     df["Amount_clean"] = df["Amount"].apply(clean_amount)
+    df["Amount_clean"] = pd.to_numeric(df["Amount_clean"], errors="coerce")
 
     for col in ["Activity Date", "Process Date", "Settle Date"]:
-        df[col] = pd.to_datetime(df[col], errors="coerce")
+        df[col] = df[col].apply(parse_date_safe)
+        # df[col] = pd.to_datetime(df[col], errors="coerce")
 
     df.to_csv("Cleaned_Trades_sample.csv", index=False)
 
